09c1d96fe5d5be88c4afe0ce2ef74b076f24df5d
AN-166 <aizatul.work@gmail.com>
2025-08-07 11:30:16 +0800

    Delete src/main directory

--- src/main/java/com/assement/customerbatch/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.assement.customerbatch.repository;
-
-import com.assement.customerbatch.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long>, JpaSpecificationExecutor<Customer> {
-}
-
